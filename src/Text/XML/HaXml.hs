cbd4a412ef54c4275081c27280d415c0a008ac65
malcolm <unknown>
2002-08-08 12:44:37 +0000

    fptools/libraries/HaXml: changeset 2002-08-08 12:44:37 by malcolm
    Change a couple of import/exports.

--- src/Text/XML/HaXml.hs
+++ src/Text/XML/HaXml.hs
@@ -8,7 +8,6 @@ module Text.Xml.HaXml
   , module Text.Xml.HaXml.Pretty
   , module Text.Xml.HaXml.Html.Generate
   , module Text.Xml.HaXml.Html.Parse
-  , module Text.Xml.HaXml.Html.Pretty
   , module Text.Xml.HaXml.Validate
   , module Text.Xml.HaXml.Wrappers
   , render
@@ -20,8 +19,8 @@ import Text.Xml.HaXml.Parse       (xmlParse,dtdParse)
 import Text.Xml.HaXml.Pretty      (element)
 import Text.Xml.HaXml.Html.Generate
 import Text.Xml.HaXml.Html.Parse  (htmlParse)
-import Text.Xml.HaXml.Html.Pretty ()
 import Text.Xml.HaXml.Validate    (validate)
+import Text.Xml.HaXml.Wrappers    (fix2Args,processXmlWith)
 
 import Text.PrettyPrint.HughesPJ  (render)
 
