c4a3c5a60c86dac7488f17691ed136f4ef8968ef
gwern0 <gwern0@gmail.com>
2008-02-10 05:08:32 +0000

    wall ./src/Text/XML/HaXml/DtdToHaskell/Instance.hs

--- src/Text/XML/HaXml/DtdToHaskell/Instance.hs
+++ src/Text/XML/HaXml/DtdToHaskell/Instance.hs
@@ -12,8 +12,8 @@ import Text.PrettyPrint.HughesPJ
 mkInstance :: TypeDef -> Doc
 
 -- no constructors - represents an element with empty content but attributes.
-mkInstance (DataDef aux n fs []) =
-    let (frpat, frattr, topat, toattr) = attrpats fs
+mkInstance (DataDef _ n fs []) =
+    let (_, frattr, topat, toattr) = attrpats fs
         frretval = if null fs then ppHName n else frattr
         topatval = if null fs then ppHName n else topat
     in
@@ -81,9 +81,9 @@ mkInstance (DataDef True n [] [(n0,sts)]) =
 
 -- multiple constructors (real)
 mkInstance (DataDef False n fs cs) =
-    let vs = nameSupply cs
+    let _ = nameSupply cs
         (frpat, frattr, topat, toattr) = attrpats fs
-        mixattrs = if null fs then False else True
+        _ = if null fs then False else True
     in
     text "instance HTypeable" <+> ppHName n <+> text "where" $$
     nest 4 ( text "toHType x = Defined \"" <> ppXName n <> text "\" [] []" )
@@ -108,8 +108,8 @@ mkInstance (DataDef False n fs cs) =
 
 -- multiple constructors (auxiliary)
 mkInstance (DataDef True n fs cs) =
-    let vs = nameSupply cs
-        (frpat, frattr, topat, toattr) = attrpats fs
+    let _ = nameSupply cs
+        (_, frattr, _, _) = attrpats fs
         mixattrs = if null fs then False else True
     in
     text "instance HTypeable" <+> ppHName n <+> text "where" $$
@@ -143,7 +143,7 @@ mkInstance (EnumDef n es) =
 data SameName = Same | Extended
 
 mkInstanceAttrs        :: SameName -> Name -> AttrFields -> Doc
-mkInstanceAttrs s n []  = empty
+mkInstanceAttrs _ _ []  = empty
 mkInstanceAttrs s n fs  =
     let ppName = case s of { Same-> ppHName;  Extended-> ppAName; }
     in
@@ -170,46 +170,46 @@ attrpats fs =
 
 
 
-mkFrElem :: Name -> [StructType] -> [Doc] -> Doc -> Doc
-mkFrElem n sts vs inner =
-    foldr (frElem n) inner (zip3 sts vs cvs)
-  where
-    cvs = let ns = nameSupply2 vs
-          in zip ns (text "c0": init ns)
-    frElem n (st,v,(cvi,cvo)) inner =
-        parens (text "\\" <> parens (v<>comma<>cvi) <> text "->" $$
-                nest 2 inner) $$
-        parens (
-          case st of
-            (Maybe String)  -> text "fromText" <+> cvo
-            (Maybe s)       -> text "fromElem" <+> cvo
-            (List String)   -> text "many fromText" <+> cvo
-            (List s)        -> text "many fromElem" <+> cvo
-            (List1 s)       -> text "definite fromElem"
-                               <+> text "\"" <> text (show s)<> text "+\""
-                               <+> text "\"" <> ppXName n <> text "\""
-                               <+> cvo
-            (Tuple ss)  -> text "definite fromElem"
-                           <+> text "\"(" <> hcat (intersperse (text ",")
-                                                           (map (text.show) ss))
-                                          <> text ")\""
-                           <+> text "\"" <> ppXName n <> text "\""
-                           <+> cvo
-            (OneOf ss)  -> text "definite fromElem"
-                           <+> text "\"OneOf\""
-                           <+> text "\"" <> ppXName n <> text "\""
-                           <+> cvo
-            (String)    -> text "definite fromText" <+> text "\"text\" \"" <>
-                                                 ppXName n <> text "\"" <+> cvo
-            (Any)       -> text "definite fromElem" <+> text "\"ANY\" \"" <>
-                                                 ppXName n <> text "\"" <+> cvo
-            (Defined m) -> text "definite fromElem" <+>
-				 text "\"<" <> ppXName m <> text ">\" \"" <>
-                                                 ppXName n <> text "\"" <+> cvo
-            (Defaultable _ _)  -> text "nyi_fromElem_Defaultable" <+> cvo
-          )
+-- mkFrElem :: Name -> [StructType] -> [Doc] -> Doc -> Doc
+-- mkFrElem n sts vs inner =
+--     foldr (frElem n) inner (zip3 sts vs cvs)
+--   where
+--     cvs = let ns = nameSupply2 vs
+--           in zip ns (text "c0": init ns)
+--     frElem _ (st,v,(cvi,cvo)) inner =
+--         parens (text "\\" <> parens (v<>comma<>cvi) <> text "->" $$
+--                 nest 2 inner) $$
+--         parens (
+--           case st of
+--             (Maybe String)  -> text "fromText" <+> cvo
+--             (Maybe _)       -> text "fromElem" <+> cvo
+--             (List String)   -> text "many fromText" <+> cvo
+--             (List _)        -> text "many fromElem" <+> cvo
+--             (List1 s)       -> text "definite fromElem"
+--                                <+> text "\"" <> text (show s)<> text "+\""
+--                                <+> text "\"" <> ppXName n <> text "\""
+--                                <+> cvo
+--             (Tuple ss)  -> text "definite fromElem"
+--                            <+> text "\"(" <> hcat (intersperse (text ",")
+--                                                            (map (text.show) ss))
+--                                           <> text ")\""
+--                            <+> text "\"" <> ppXName n <> text "\""
+--                            <+> cvo
+--             (OneOf _)  -> text "definite fromElem"
+--                            <+> text "\"OneOf\""
+--                            <+> text "\"" <> ppXName n <> text "\""
+--                            <+> cvo
+--             (String)    -> text "definite fromText" <+> text "\"text\" \"" <>
+--                                                  ppXName n <> text "\"" <+> cvo
+--             (Any)       -> text "definite fromElem" <+> text "\"ANY\" \"" <>
+--                                                  ppXName n <> text "\"" <+> cvo
+--             (Defined m) -> text "definite fromElem" <+>
+--                                  text "\"<" <> ppXName m <> text ">\" \"" <>
+--                                                  ppXName m <> text "\"" <+> cvo
+--             (Defaultable _ _)  -> text "nyi_fromElem_Defaultable" <+> cvo
+--           )
 
--- 
+--
 {-
 mkParseContents :: Name -> [StructType] -> [Doc] -> Doc -> Doc
 mkParseContents n sts vs inner =
@@ -223,24 +223,25 @@ mkParseContents n sts vs inner =
         parens (
           )
 -}
-
+mkParseConstr :: Doc -> (Name, [StructType]) -> Doc
 mkParseConstr frattr (c,sts) =
         fsep (text "return" <+> parens (ppHName c <+> frattr)
              : map mkParseContents sts)
 
+mkParseContents :: StructType -> Doc
 mkParseContents st =
   let ap = text "`apply`" in
           case st of
             (Maybe String)  -> ap <+> text "optional text"
-            (Maybe s)       -> ap <+> text "optional parseContents"
+            (Maybe _)       -> ap <+> text "optional parseContents"
             (List String)   -> ap <+> text "many text"
-            (List s)        -> ap <+> text "many parseContents"
-            (List1 s)       -> ap <+> text "parseContents"
-            (Tuple ss)      -> ap <+> text "parseContents"
-            (OneOf ss)      -> ap <+> text "parseContents"
+            (List _)        -> ap <+> text "many parseContents"
+            (List1 _)       -> ap <+> text "parseContents"
+            (Tuple _)      -> ap <+> text "parseContents"
+            (OneOf _)      -> ap <+> text "parseContents"
             (String)        -> ap <+> text "(text `onFail` return \"\")"
             (Any)           -> ap <+> text "parseContents"
-            (Defined m)     -> ap <+> text "parseContents"
+            (Defined _)     -> ap <+> text "parseContents"
             (Defaultable _ _)  -> ap <+> text "nyi_fromElem_Defaultable"
 
 --
@@ -252,27 +253,27 @@ mkToElem sts vs =
     toElem st v =
       case st of
         (Maybe String)  -> text "maybe [] toText" <+> v
-        (Maybe s)       -> text "maybe [] toContents" <+> v
+        (Maybe _)       -> text "maybe [] toContents" <+> v
         (List String)   -> text "concatMap toText" <+> v
-        (List s)        -> text "concatMap toContents" <+> v
-        (List1 s)       -> text "toContents" <+> v
-        (Tuple ss)      -> text "toContents" <+> v
-        (OneOf ss)      -> text "toContents" <+> v
+        (List _)        -> text "concatMap toContents" <+> v
+        (List1 _)       -> text "toContents" <+> v
+        (Tuple _)      -> text "toContents" <+> v
+        (OneOf _)      -> text "toContents" <+> v
         (String)        -> text "toText" <+> v
         (Any)           -> text "toContents" <+> v
-        (Defined m)     -> text "toContents" <+> v
+        (Defined _)     -> text "toContents" <+> v
         (Defaultable _ _)  -> text "nyi_toElem_Defaultable" <+> v
 
-mkRpat :: [Doc] -> Doc
-mkRpat [v] = v
-mkRpat vs  = (parens . hcat . intersperse comma) vs
+-- mkRpat :: [Doc] -> Doc
+-- mkRpat [v] = v
+-- mkRpat vs  = (parens . hcat . intersperse comma) vs
 
 mkCpat :: Name -> Doc -> [Doc] -> Doc
 mkCpat n i vs = ppHName n <+> i <+> fsep vs
 
-nameSupply,nameSupply2 :: [b] -> [Doc]
+nameSupply :: [b] -> [Doc]
 nameSupply  ss = take (length ss) (map char ['a'..])
-nameSupply2 ss = take (length ss) [ text ('c':v:[]) | v <- ['a'..]]
+-- nameSupply2 ss = take (length ss) [ text ('c':v:[]) | v <- ['a'..]]
 
 mkTranslate :: [Name] -> Doc
 mkTranslate es =
@@ -282,6 +283,7 @@ mkTranslate es =
     trans n = text "translate \"" <> ppXName n <> text "\" =" <+>
               text "Just" <+> ppHName n
 
+mkToAttr :: Name -> Doc
 mkToAttr n = text "toAttrFrTyp n" <+> ppHName n <+> text "=" <+>
              text "Just (n, str2attr" <+> doubleQuotes (ppXName n) <> text ")"
 
@@ -290,14 +292,14 @@ mkFrFld tag (n,st) =
     ppHName n <+> text "=" <+>
     ( case st of
         (Defaultable String s) -> text "defaultA fromAttrToStr" <+>
-						 doubleQuotes (text s)
+                                                 doubleQuotes (text s)
         (Defaultable _ s)      -> text "defaultA fromAttrToTyp" <+> text s
         (Maybe String)         -> text "possibleA fromAttrToStr"
         (Maybe _)              -> text "possibleA fromAttrToTyp"
         String                 -> text "definiteA fromAttrToStr" <+>
-						 doubleQuotes (ppXName tag)
+                                                 doubleQuotes (ppXName tag)
         _                      -> text "definiteA fromAttrToTyp" <+>
-						 doubleQuotes (ppXName tag)
+                                                 doubleQuotes (ppXName tag)
     ) <+> doubleQuotes (ppXName n) <+> text "as"
 
 mkToFld :: (Name,StructType) -> Doc
@@ -311,67 +313,67 @@ mkToFld (n,st) =
         _                      -> text "toAttrFrTyp"
     ) <+> doubleQuotes (ppXName n) <+> parens (ppHName n <+> text "v")
 
-mkFrAux :: Bool -> Doc -> [(Name,[StructType])] -> Doc
-mkFrAux keeprest attrs cs = foldr frAux inner cs
-  where
-    inner = text "(Nothing, c0)"
-    rest  = if keeprest then text "rest" else text "_"
-    frAux (n,sts) inner =
-        let vs  = nameSupply sts in
-        nest 4 (text "case" <+> blah sts vs <+> text "of" $$
-                succpat sts vs <+> text "-> (Just" <+>
-                                   parens (mkCpat n attrs vs) <> text ", rest)"
-                $$
-                failpat sts <+> text "->" $$ nest 4 inner
-               )
-    blah [st] [v] =
-        blahblahblah st (text "c0")
-    blah sts vs =
-        let ns = nameSupply2 vs
-            cvs = zip ns (text "c0": init ns)
-            blahblah (st,v,(cvi,cvo)) inner =
-                parens (text "\\" <> parens (v<>comma<>cvi) <> text "->" $$
-                        nest 2 inner) $$
-                blahblahblah st cvo
-        in
-        foldr blahblah (mkRpat (vs++[last ns])) (zip3 sts vs cvs)
-    blahblahblah st cvo = parens (
-        case st of
-          (Maybe String) -> text "fromText" <+> cvo
-          (Maybe s)      -> text "fromElem" <+> cvo
-          (List String)  -> text "many fromText" <+> cvo
-          (List s)       -> text "many fromElem" <+> cvo
-          (List1 s)      -> text "fromElem" <+> cvo
-          (Tuple ss)     -> text "fromElem" <+> cvo	-- ??
-          (OneOf ss)     -> text "fromElem" <+> cvo
-          (String)       -> text "fromText" <+> cvo
-          (Any)          -> text "fromElem" <+> cvo
-          (Defined m)    -> text "fromElem" <+> cvo
-        )
-    failpat sts =
-        let fp st =
-                case st of
-                  (Maybe s)   -> text "Nothing"
-                  (List s)    -> text "[]"
-                  (List1 s)   -> text "_"
-                  (Tuple ss)  -> text "_"
-                  (OneOf ss)  -> text "_"
-                  (String)    -> text "_"
-                  (Any)       -> text "_"
-                  (Defined m) -> text "_"
-        in parens (hcat (intersperse comma (map fp sts++[text "_"])))
-    succpat sts vs =
-        let sp st v =
-                case st of
-                  (Maybe s)   -> v
-                  (List s)    -> v
-                  (List1 s)   -> text "Just" <+> v
-                  (Tuple ss)  -> text "Just" <+> v
-                  (OneOf ss)  -> text "Just" <+> v
-                  (String)    -> text "Just" <+> v
-                  (Any)       -> text "Just" <+> v
-                  (Defined m) -> text "Just" <+> v
-        in parens (hcat (intersperse comma (zipWith sp sts vs++[rest])))
+-- mkFrAux :: Bool -> Doc -> [(Name,[StructType])] -> Doc
+-- mkFrAux keeprest attrs cs = foldr frAux inner cs
+--   where
+--     inner = text "(Nothing, c0)"
+--     rest  = if keeprest then text "rest" else text "_"
+--     frAux (n,sts) innr =
+--         let vs  = nameSupply sts in
+--         nest 4 (text "case" <+> blah sts vs <+> text "of" $$
+--                 succpat sts vs <+> text "-> (Just" <+>
+--                                    parens (mkCpat n attrs vs) <> text ", rest)"
+--                 $$
+--                 failpat sts <+> text "->" $$ nest 4 innr
+--                )
+--     blah [st] [_] =
+--         blahblahblah st (text "c0")
+--     blah sts vs =
+--         let ns = nameSupply2 vs
+--             cvs = zip ns (text "c0": init ns)
+--             blahblah (st,v,(cvi,cvo)) innr =
+--                 parens (text "\\" <> parens (v<>comma<>cvi) <> text "->" $$
+--                         nest 2 innr) $$
+--                 blahblahblah st cvo
+--         in
+--         foldr blahblah (mkRpat (vs++[last ns])) (zip3 sts vs cvs)
+--     blahblahblah st cvo = parens (
+--         case st of
+--           (Maybe String) -> text "fromText" <+> cvo
+--           (Maybe _)      -> text "fromElem" <+> cvo
+--           (List String)  -> text "many fromText" <+> cvo
+--           (List _)       -> text "many fromElem" <+> cvo
+--           (List1 _)      -> text "fromElem" <+> cvo
+--           (Tuple _)     -> text "fromElem" <+> cvo      -- ??
+--           (OneOf _)     -> text "fromElem" <+> cvo
+--           (String)       -> text "fromText" <+> cvo
+--           (Any)          -> text "fromElem" <+> cvo
+--           (Defined _)    -> text "fromElem" <+> cvo
+--         )
+--     failpat sts =
+--         let fp st =
+--                 case st of
+--                   (Maybe _)   -> text "Nothing"
+--                   (List _)    -> text "[]"
+--                   (List1 _)   -> text "_"
+--                   (Tuple _)  -> text "_"
+--                   (OneOf _)  -> text "_"
+--                   (String)    -> text "_"
+--                   (Any)       -> text "_"
+--                   (Defined _) -> text "_"
+--         in parens (hcat (intersperse comma (map fp sts++[text "_"])))
+--     succpat sts vs =
+--         let sp st v =
+--                 case st of
+--                   (Maybe _)   -> v
+--                   (List _)    -> v
+--                   (List1 _)   -> text "Just" <+> v
+--                   (Tuple _)  -> text "Just" <+> v
+--                   (OneOf _)  -> text "Just" <+> v
+--                   (String)    -> text "Just" <+> v
+--                   (Any)       -> text "Just" <+> v
+--                   (Defined _) -> text "Just" <+> v
+--         in parens (hcat (intersperse comma (zipWith sp sts vs++[rest])))
 
 mkToAux :: Bool -> (Name,[StructType]) -> Doc
 mkToAux mixattrs (n,sts) =
