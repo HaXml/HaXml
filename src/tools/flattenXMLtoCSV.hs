4ab120fe56e2f5dda2a21f1f381cc542b93d4392
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2009-12-04 08:05:03 +0000

    New example of a simple command-line tool: flattenXMLtoCSV.
    It depends on the lazy-csv package (not yet noted in the .cabal file).
    Basic idea is to create a two-dimensional CSV table by selecting
    (xtracting) one level of nested element for the rows, and a sequence of
    elements further-nested within it for the columns.

---
+++ src/tools/flattenXMLtoCSV.hs
@@ -0,0 +1,59 @@
+------------------------------------------------------------
+-- The Xtract tool - an XML-grep.
+------------------------------------------------------------ 
+module Main where
+import System (getArgs, exitWith, ExitCode(..))
+import IO
+import Char         (toLower)
+import List         (isSuffixOf)
+
+import Text.CSV.Lazy.String
+
+import Text.XML.HaXml.Types
+import Text.XML.HaXml.Posn          (posInNewCxt,Posn)
+import Text.XML.HaXml.ParseLazy     (xmlParse)
+import Text.XML.HaXml.Xtract.Parse  (xtract)
+import Text.XML.HaXml.Pretty        (content)
+import Text.XML.HaXml.Escape        (xmlUnEscapeContent,stdXmlEscaper)
+import Text.XML.HaXml.Verbatim      (verbatim)
+
+main :: IO ()
+main =
+  getArgs >>= \args->
+  if length args < 4 then do
+    putStrLn "Usage: flattenXMLtoCSV file.xml file.csv tag innertag [innertag ...]  "
+    exitWith (ExitFailure 1)
+  else do
+    let (ifile:ofile:tag:patterns) = args
+    -- 'patterns' is the header row of the csv file, consisting of tag names
+    -- to be found nested inside the specified outer 'tag'.  Each outer 'tag'
+    -- found therefore forms a row of the csv output.
+    writeFile (csv ofile) . ppCSVTable
+                          . ((map (mkCSVField 0 0) patterns):)
+                          . map (\o-> map (\i-> mkCSVField 0 0
+                                                . verbatim
+                                                . unescape
+                                                . xtract id ("//"++i++"/-")
+                                                $ o)
+                                          patterns)
+                          . xtract id ("//"++tag)
+                          . getElem ifile . xmlParse ifile =<< readFile ifile
+
+getElem :: String -> Document Posn -> Content Posn
+getElem x (Document _ _ e _) = CElem e (posInNewCxt x Nothing)
+
+csv :: FilePath -> FilePath
+csv fp | ".xml" `isSuffixOf` fp = (++".csv") . reverse . drop 4 . reverse $ fp
+       | ".csv" `isSuffixOf` fp = fp
+       | otherwise              = fp++".csv"
+
+unescape :: [Content i] -> [Content i]
+unescape = xmlUnEscapeContent stdXmlEscaper
+
+{-
+checkOne :: [Content i] -> String
+checkOne []              = ""
+checkOne [CString _ x _] = x
+checkOne _               = verbatim
+-}
+
