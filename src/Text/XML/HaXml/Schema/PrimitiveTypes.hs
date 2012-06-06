b34bae222f6dc02c463811d323f02e0ec54001ec
malcolm.wallace <malcolm.wallace@me.com>
2012-06-06 09:17:21 +0000

    Schema: extend the SimpleType class to allow conversion to text.

--- src/Text/XML/HaXml/Schema/PrimitiveTypes.hs
+++ src/Text/XML/HaXml/Schema/PrimitiveTypes.hs
@@ -66,6 +66,7 @@ import Data.Word
 --   as layered parsers.
 class SimpleType a where
   acceptingParser :: TextParser a
+  simpleTypeText  :: a -> String
 
 -- * Primitive types
 
@@ -106,25 +107,35 @@ instance SimpleType Bool where
                                    "0"     -> return False;
                                    "1"     -> return True
                                    _       -> fail ("Not a bool: "++w)
+    simpleTypeText False = "false"
+    simpleTypeText True  = "true"
 instance SimpleType XsdString where
     acceptingParser = fmap XsdString (many next)
+    simpleTypeText (XsdString s) = s
 instance SimpleType Base64Binary where
     acceptingParser = fmap Base64Binary (many (satisfy isAlphaNum `onFail`
                                                satisfy isSpace `onFail`
                                                satisfy (`elem`"+/=")))
+    simpleTypeText (Base64Binary s) = s
 instance SimpleType HexBinary where
     acceptingParser = fmap HexBinary (many (satisfy Char.isHexDigit))
+    simpleTypeText (HexBinary s) = s
 instance SimpleType AnyURI where
     acceptingParser = fmap AnyURI (many next) -- not very satisfactory
+    simpleTypeText (AnyURI s) = s
 instance SimpleType NOTATION where
     acceptingParser = fmap NOTATION (many next) -- not very satisfactory
+    simpleTypeText (NOTATION s) = s
 
 instance SimpleType Decimal where
     acceptingParser = fmap Decimal parse
+    simpleTypeText (Decimal s) = show s
 instance SimpleType Float where
-    acceptingParser = parse
+    acceptingParser  = parse
+    simpleTypeText x = show x
 instance SimpleType Double where
-    acceptingParser = parse
+    acceptingParser  = parse
+    simpleTypeText x = show x
 
 instance SimpleType Duration where
     acceptingParser = return Duration `apply` (do isNext '-'; return False
@@ -144,31 +155,42 @@ instance SimpleType Duration where
                                                `onFail` return 0)
                                       `apply` ((parseFloat `discard` isNext 'S')
                                                `onFail` return 0)
+    simpleTypeText (Duration pos y m d h n s) =
+        (if pos then "" else "-")++show y++"Y"++show m++"M"++show d++"D"
+        ++"T"++show h++"H"++show n++"M"++show s++"S"
 
 instance SimpleType DateTime where
     acceptingParser = fmap DateTime (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for DateTime"
+    simpleTypeText (DateTime x) = x
 instance SimpleType Time where
     acceptingParser = fmap Time (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for Time"
+    simpleTypeText (Time x) = x
 instance SimpleType Date where
     acceptingParser = fmap Date (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for Date"
+    simpleTypeText (Date x) = x
 instance SimpleType GYearMonth where
     acceptingParser = fmap GYearMonth (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for GYearMonth"
+    simpleTypeText (GYearMonth x) = x
 instance SimpleType GYear where
     acceptingParser = fmap GYear (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for GYear"
+    simpleTypeText (GYear x) = x
 instance SimpleType GMonthDay where
     acceptingParser = fmap GMonthDay (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for GMonthDay"
+    simpleTypeText (GMonthDay x) = x
 instance SimpleType GDay where
     acceptingParser = fmap GDay (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for GDay"
+    simpleTypeText (GDay x) = x
 instance SimpleType GMonth where
     acceptingParser = fmap GMonth (many next)
  -- acceptingParser = fail "not implemented: simpletype parser for GMonth"
+    simpleTypeText (GMonth x) = x
 
 -- * Derived builtin types
 
@@ -187,28 +209,40 @@ newtype NMTOKENS = NMTOKENS String              deriving (Eq,Show)
 
 instance SimpleType NormalizedString where
     acceptingParser = fmap Normalized (many next)
+    simpleTypeText (Normalized x) = x
 instance SimpleType Token where
     acceptingParser = fmap Token (many next)
+    simpleTypeText (Token x) = x
 instance SimpleType Language where
     acceptingParser = fmap Language (many next)
+    simpleTypeText (Language x) = x
 instance SimpleType Name where
     acceptingParser = fmap Name (many next)
+    simpleTypeText (Name x) = x
 instance SimpleType NCName where
     acceptingParser = fmap NCName (many next)
+    simpleTypeText (NCName x) = x
 instance SimpleType ID where
     acceptingParser = fmap ID (many next)
+    simpleTypeText (ID x) = x
 instance SimpleType IDREF where
     acceptingParser = fmap IDREF (many next)
+    simpleTypeText (IDREF x) = x
 instance SimpleType IDREFS where
     acceptingParser = fmap IDREFS (many next)
+    simpleTypeText (IDREFS x) = x
 instance SimpleType ENTITY where
     acceptingParser = fmap ENTITY (many next)
+    simpleTypeText (ENTITY x) = x
 instance SimpleType ENTITIES where
     acceptingParser = fmap ENTITIES (many next)
+    simpleTypeText (ENTITIES x) = x
 instance SimpleType NMTOKEN where
     acceptingParser = fmap NMTOKEN (many next)
+    simpleTypeText (NMTOKEN x) = x
 instance SimpleType NMTOKENS where
     acceptingParser = fmap NMTOKENS (many next)
+    simpleTypeText (NMTOKENS x) = x
 
 --data Integer
 newtype NonPositiveInteger = NonPos   Integer   deriving (Eq,Show)
@@ -226,28 +260,41 @@ newtype PositiveInteger    = Positive Integer   deriving (Eq,Show)
 
 instance SimpleType Integer where
     acceptingParser = parse
+    simpleTypeText  = show
 instance SimpleType NonPositiveInteger where
     acceptingParser = fmap NonPos parse
+    simpleTypeText (NonPos x) = show x
 instance SimpleType NegativeInteger where
     acceptingParser = fmap Negative parse
+    simpleTypeText (Negative x) = show x
 instance SimpleType Long where
     acceptingParser = fmap (Long . fromInteger) parse
+    simpleTypeText (Long x) = show x
 instance SimpleType Int where
     acceptingParser = parse
+    simpleTypeText  = show
 instance SimpleType Short where
     acceptingParser = fmap (Short . fromInteger) parse
+    simpleTypeText (Short x) = show x
 instance SimpleType Byte where
     acceptingParser = fmap (Byte . fromInteger) parse
+    simpleTypeText (Byte x) = show x
 instance SimpleType NonNegativeInteger where
     acceptingParser = fmap NonNeg parse
+    simpleTypeText (NonNeg x) = show x
 instance SimpleType UnsignedLong where
     acceptingParser = fmap (ULong . fromInteger) parse
+    simpleTypeText (ULong x) = show x
 instance SimpleType UnsignedInt where
     acceptingParser = fmap (UInt . fromInteger) parse
+    simpleTypeText (UInt x) = show x
 instance SimpleType UnsignedShort where
     acceptingParser = fmap (UShort . fromInteger) parse
+    simpleTypeText (UShort x) = show x
 instance SimpleType UnsignedByte where
     acceptingParser = fmap (UByte . fromInteger) parse
+    simpleTypeText (UByte x) = show x
 instance SimpleType PositiveInteger where
     acceptingParser = fmap Positive parse
+    simpleTypeText (Positive x) = show x
 
